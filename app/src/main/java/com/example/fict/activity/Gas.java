c7185fd1c07477694285640d913f2e9606d32b32
VladislavTiku <bigvladis1999@gmail.com>
2019-03-10 18:10:44 +0200

    Syn v0.5

--- app/src/main/java/com/example/fict/activity/Gas.java
+++ app/src/main/java/com/example/fict/activity/Gas.java
@@ -1,15 +1,32 @@
 package com.example.fict.activity;
 
+
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.fict.R;
+import com.example.fict.fragments.Day_fragment;
 
-public class Gas extends AppCompatActivity {
+public class Gas extends AppCompatActivity  {
+    Day_fragment day_fragment;
+
+    Button day,week,mouth,year;
+    Fragment frag1;
+    FragmentTransaction fTrans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gas);
+        day = findViewById(R.id.day);
+        day_fragment = new Day_fragment();
     }
+
+
+
+
 }
--- app/src/main/java/com/example/fict/fragments/Day_fragment.java
+++ app/src/main/java/com/example/fict/fragments/Day_fragment.java
@@ -6,22 +6,64 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.fict.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+
+import lecho.lib.hellocharts.model.Line;
 
 
-public class Day_fragment extends Fragment {
+public class Day_fragment extends Fragment  {
+
+    EditText textActivity;
+    Button buttonSendToFragment;
+    Fragment myFragment;
 
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private LineChart lineChart;
 
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(false);
+        ArrayList<Entry> yValue = new ArrayList<>();
+        yValue.add(new Entry(0,60f));
+        yValue.add(new Entry(1,50f));
+        yValue.add(new Entry(2,40f));
+        yValue.add(new Entry(3,60f));
+        yValue.add(new Entry(4,30f));
+        yValue.add(new Entry(5,90f));
+        yValue.add(new Entry(6,50f));
+
+        LineDataSet set1 = new LineDataSet(yValue, "Data set 1");
+
+
+        set1.setFillAlpha(110);
+
+
+
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+
+
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        lineChart.setData(data);
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_day, container, false);
 
 
